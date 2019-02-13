282f3d8ceb45de8d666e3ea87c2284e698cee22b
Vo Minh Tuan <tuan.vo@dsquare.com.vn>
2019-02-13 16:13:26 +0700

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Navigation from '../Navigation'
+import LandingPage from '../Landing'
+import SignUpPage from '../SignUp'
+import SignInPage from '../SignIn'
+import PasswordForgetPage from '../PasswordForget'
+import HomePage from '../Home'
+import AccountPage from '../Account'
+import AdminPage from '../Admin'
+import * as ROUTES from '../../constants/routes'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navigation/>
+        <hr/>
+        <Switch>
+          <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage}/>
+          <Route exact path={ROUTES.SIGN_IN} component={SignInPage}/>
+          <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage}/>
+          <Route exact path={ROUTES.HOME} component={HomePage}/>
+          <Route exact path={ROUTES.ADMIN} component={AdminPage}/>
+          <Route exact path={ROUTES.ACCOUNT} component={AccountPage}/>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
